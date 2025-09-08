b329125e8b60c6e049e014dedacbca10fc4847cb
Lucian45T <155108954+Lucian45T@users.noreply.github.com>
2025-09-08 23:21:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let grid = [
     [7, 8, 9]
 ];
 
-let y = 0, x = 0, verify = 0, saved = 0, onePush = 0, check = 0;
+let y = 0, x = 0, verify = 0, verify2 = 0, saved = 0, onePush = 0, check = 0;
 
 function generateGrid() {
     if (onePush == 0) {
@@ -12,8 +12,8 @@ function generateGrid() {
             for (let j = 0; j < 3; ++j) {
                 ++saved;
                 document.getElementById("container").innerHTML += `
-                <input type="text" id="${saved}" style="width: 50px; height: 50px;
-                 background-color: rgba(0, 255, 68, 0.1)">`;
+                    <input type="text" id="${saved}" style="width: 50px; height: 50px;
+                     background-color: rgba(0, 255, 68, 0.1)">`;
             }
             document.getElementById("container").innerHTML += `<br>`;
         }
@@ -32,14 +32,16 @@ let gridDirections = [
     [[0, 2], [1, 1], [2, 0]]
 ];
           
-function gamerX() { 
-    verify = 0; 
+function gamer() {  
     y = 0;
     for (let i = 0; i < 3; ++i) {
         for (let j = 0; j < 3; ++j) {
             ++y;
             if (document.getElementById(y).value == "x") {
                 grid[i][j] = "x";    
+            }
+            if (document.getElementById(y).value == "0") {
+                grid[i][j] = "0";    
             }
         }
     }
@@ -48,34 +50,20 @@ function gamerX() {
         if (gridDirection.every(([i, j]) =>grid[i][j] === "x")) {
             verify++; 
         }
+        if (gridDirection.every(([i, j]) =>grid[i][j] === "0")) {
+            verify2++; 
+        }
     }
+     
     
     if(verify > 0) {  
         document.getElementById("verdict").textContent = "gamer with X win";
-    }   
+    } 
+    if(verify2 > 0) {
+        document.getElementById("verdict").textContent = "gamer with 0 win";
+    }     
 }
 
-function gamer0() {
-    verify = 0;
-    y = 0;
-    for (let i = 0; i < 3; ++i) {
-        for (let j = 0; j < 3; ++j) {
-            ++y;
-            if (document.getElementById(y).value == "0") {
-                grid[i][j] = "0";   
-            }
-        }
-    }
-     for (let gridDirection of gridDirections) {
-        if (gridDirection.every(([i, j]) =>grid[i][j] === "0")) {
-            verify++; 
-        }
-    }
-    
-    if(verify > 0) {
-        document.getElementById("verdict").textContent = "gamer with 0 win";
-    }   
-}
 
 function reset() {
     x = 0; 
